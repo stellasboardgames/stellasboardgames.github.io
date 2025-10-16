fce3fab091b6f80f08c4f17e54481ae090009445
Stella OConnor <smoconnor2001@gmail.com>
2025-10-16 18:27:04 -0400

    connected google web app to project

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,11 @@ import './App.css'
 
 function App() {
   
+  fetch("https://script.google.com/macros/s/AKfycbxDimh08Xsdvla7ntKSCXkKEKyvxqO-o7hzfkQUowjf7izSCyJeoC-RZEnqs1WEEr4H/exec")
+  .then(response => response.json())
+  .then(json => {
+    console.log(json)
+  })
 
   return (
     <>
